e2baff10663c9951e2cbb7307ca66f9aff6fec30
Phong <phongdream182000@gmail.com>
2020-07-25 10:41:13 +0700

    Update

--- QLBHNongSan/resources/views/api-admin/Modules/sale/edit.blade.php
+++ QLBHNongSan/resources/views/api-admin/Modules/sale/edit.blade.php
@@ -5,12 +5,6 @@
 <div class="card">
     <div class="card-header">
         <h3 class="card-title">Sửa thông tin khuyến mãi</h3>
-        <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-            <i class="fas fa-minus"></i></button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-            <i class="fas fa-times"></i></button>
-        </div>
     </div>
     <div class="card-body">
         <form action="{{route('admin.sale.update', ['id' => $khuyenmai->id])}}" method="POST" enctype="multipart/form-data">
